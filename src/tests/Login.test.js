86e25eea100c04ee31a054dd780bf7bbf2167dbc
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-04-26 17:29:07 -0300

    Req 3

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 
 describe('2. Verifica se existe email, password e login', () => {
@@ -10,12 +11,22 @@ describe('2. Verifica se existe email, password e login', () => {
   });
   it('Verifica se existe campo do password', () => {
     render(<Login />);
-    const inputEmail = screen.getByTestId('password-input');
-    expect(inputEmail).toBeInTheDocument();
+    const inputPassword = screen.getByTestId('password-input');
+    expect(inputPassword).toBeInTheDocument();
   });
   it('Verifica se existe o botão', () => {
     render(<Login />);
-    const inputEmail = screen.getByTestId('login-submit-btn');
-    expect(inputEmail).toBeInTheDocument();
+    const btnSubmit = screen.getByTestId('login-submit-btn');
+    expect(btnSubmit).toBeInTheDocument();
+  });
+});
+
+describe('3. A pessoa deve conseguir digitar o seu email', () => {
+  it('É possivel digitar o email', () => {
+    render(<Login />);
+    const email = 'email@email.com';
+    const inputEmail = screen.getByTestId('email-input');
+    userEvent.type(inputEmail, email);
+    expect(screen.getByRole('textbox')).toHaveValue(email);
   });
 });
