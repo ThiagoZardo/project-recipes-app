854399dcccbc1c8da748eeb04feba3b7911b2058
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-04-26 22:30:03 -0300

    Req 13

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import SearchBar from '../components/SearchBar';
 import renderWithRouter from '../renderWithRouter';
 
