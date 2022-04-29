9de789eeb628acc8e208d681d8eef0df3ba02190
Terexes <rafamoreci@gmail.com>
2022-04-29 18:20:24 -0300

    Merge + resolução de conflitos

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { getSurpriseDrinks } from '../helpers';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
+  const btnSurpriseDrink = async () => {
+    const dataGetDrinks = await getSurpriseDrinks();
+    const idDrinkRandom = dataGetDrinks.drinks[0].idDrink;
+    console.log(idDrinkRandom);
+    history.push(`/drinks/${idDrinkRandom}`);
+  };
+
   return (
     <div>
       <header>
         <Header heading="Explore Drinks" />
       </header>
+      <div>
+        <header>
+          <Header heading="Explore Foods" />
+        </header>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/drinks/ingredients') }
+        >
+          By Ingredient
+        </button>
+
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ btnSurpriseDrink }
+        >
+          Surprise me!
+        </button>
+
+        <Footer />
+      </div>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { getSurpriseFoods } from '../helpers';
 
 function ExploreFoods() {
+  const history = useHistory();
+
+  const btnSurpriseFood = async () => {
+    const dataGetFoods = await getSurpriseFoods();
+    const idFoodRandom = dataGetFoods.meals[0].idMeal;
+    history.push(`/foods/${idFoodRandom}`);
+  };
+
   return (
     <div>
       <header>
         <Header heading="Explore Foods" />
       </header>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ btnSurpriseFood }
+      >
+        Surprise me!
+      </button>
+
       <Footer />
     </div>
   );
